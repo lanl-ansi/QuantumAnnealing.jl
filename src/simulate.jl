e37871b01a9ec613e0bc883a7bf96e211826221f
Carleton Coffrin <carleton@coffrin.com>
2022-03-07 15:11:57 -0700

    draft hardcoded o4, confirmed generic method is working up to o4

--- src/simulate.jl
+++ src/simulate.jl
@@ -312,6 +312,180 @@ end
 
 
 
+function simulate_o4(ising_model::Dict, annealing_time::Real, annealing_schedule::AnnealingSchedule, steps::Int; initial_state=nothing, constant_field_x=nothing, constant_field_z=nothing, state_steps=nothing)
+    if steps < 2
+        error("at least two steps are required by simulate, given $(steps)")
+    end
+
+    n = _check_ising_model_ids(ising_model)
+
+    if initial_state == nothing
+        initial_state = annealing_schedule.init_default(n)
+    end
+
+    if constant_field_x == nothing
+        constant_field_x = zeros(n)
+    end
+
+    if constant_field_z == nothing
+        constant_field_z = zeros(n)
+    end
+
+    track_states = !(state_steps == nothing)
+
+    t0 = 0
+    s0 = 0
+
+    R0 = initial_state * initial_state'
+
+    ηs = ones(n)
+    hs = zeros(n)
+
+    x_component = sum_x(n)
+    z_component = SparseArrays.spzeros(2^n, 2^n)
+    for (tup,w) in ising_model
+        z_component = z_component + sum_z_tup(n, tup, w)
+    end
+
+    xz_bracket = lie_bracket(x_component, z_component)
+
+    constant_component = sum_x(n, constant_field_x) + sum_z(n, constant_field_z)
+    constant_bracket_x = lie_bracket(x_component, constant_component)
+    constant_bracket_z = lie_bracket(z_component, constant_component)
+
+    s_steps = range(0, 1, length=steps)
+    R_current = R0
+    U = foldl(kron, [IMAT for i = 1:n])
+
+    if track_states
+        push!(state_steps, R_current)
+    end
+
+    for i in 1:(steps-1)
+        s0 = s_steps[i]
+        s1 = s_steps[i+1]
+        δs = s1 - s0
+
+        a_2, a_1, a_0 = get_function_coefficients(annealing_schedule.A, s0, s1)
+        b_2, b_1, b_0 = get_function_coefficients(annealing_schedule.B, s0, s1)
+
+        a_2_shift = a_2
+        a_1_shift = a_1 + 2*a_2*s0
+        a_0_shift = a_0 + a_1*s0 + a_2*s0^2
+
+        b_2_shift = b_2
+        b_1_shift = b_1 + 2*b_2*s0
+        b_0_shift = b_0 + b_1*s0 + b_2*s0^2
+
+        # H = [
+        #     (poly=[a_0_shift,a_1_shift,a_2_shift], matrix=Matrix(-im*annealing_time .* x_component)),
+        #     (poly=[b_0_shift,b_1_shift,b_2_shift], matrix=Matrix(-im*annealing_time .* z_component))
+        # ]
+
+        # Ω_list = _magnus_generator(H, order)
+        # Ω = sum(_hamiltonian_eval(δs, Ωi) for Ωi in Ω_list)
+
+        a_2_shift2 = δs^2*a_2_shift
+        a_1_shift2 = δs*a_1_shift
+        a_0_shift2 = a_0_shift
+
+        b_2_shift2 = δs^2*b_2_shift
+        b_1_shift2 = δs*b_1_shift
+        b_0_shift2 = b_0_shift
+
+        Q21 = lie_bracket(x_component, z_component)
+        Q31 = lie_bracket(x_component, Q21)
+        Q32 = lie_bracket(Q21, z_component)
+        Q41 = lie_bracket(x_component, Q31)
+        Q42 = lie_bracket(x_component, Q32)
+        Q43 = lie_bracket(z_component, Q32)
+
+        a_vec = [a_0_shift2, a_1_shift2, a_2_shift2]
+        b_vec = [b_0_shift2, b_1_shift2, b_2_shift2]
+
+        δst = δs*annealing_time
+        Ω1 = -im*δst*(_int1(a_vec)*x_component + _int1(b_vec)*z_component)
+        Ω2 = -im*δst^2/2*(_int21(a_vec,b_vec)*Q21)
+        Ω3 = -im*δst^3/6*(_int31(a_vec,b_vec)*Q31 + _int31(b_vec,a_vec)*Q32)
+        #Ω4 = -im*δst^4/6*(_int41(a_vec,b_vec)*Q41 + _int42(a_vec,b_vec)*Q42 + _int41(b_vec,a_vec)*Q43)
+
+        display(Matrix(Ω1))
+        display(Matrix(Ω2))
+        display(Matrix(Ω3))
+        #display(Matrix(Ω4))
+
+
+        #U_next = exp(Matrix(Ω1))
+        #U_next = exp(Matrix(Ω1 + Ω2))
+        U_next = exp(Matrix(Ω1 + Ω2 + Ω3))
+        #U_next = exp(Matrix(Ω1 + Ω2 + Ω3 + Ω4))
+
+        U = U_next * U
+
+        if track_states
+            R_current = U * R0 * U'
+            push!(state_steps, R_current)
+        end
+    end
+
+    return U * R0 * U'
+end
+
+
+function _int1(u::Vector)
+    return u[1] + u[2]/2 + u[3]/3
+end
+
+function _int21(u::Vector, v::Vector)
+    return 1/6*u[2]*v[1] + 1/6*u[3]*v[1] - 1/6*u[1]*v[2] + 1/30*u[3]*v[2] -
+        1/6*u[1]*v[3] - 1/30*u[2]*v[3]
+end
+
+function _int31(u::Vector, v::Vector)
+    return 1/40*u[2]^2*v[1] - 1/60*u[1]*u[3]*v[1] + 1/24*u[2]*u[3]*v[1] +
+        5/252*u[3]^2*v[1] - 1/40*u[1]*u[2]*v[2] - 1/30*u[1]*u[3]*v[2] +
+        1/840*u[2]*u[3]*v[2] + 1/360*u[3]^2*v[2] + 1/60*u[1]^2*v[3] -
+        1/120*u[1]*u[2]*v[3] - 1/840*u[2]^2*v[3] - 5/252*u[1]*u[3]*v[3] -
+        1/360*u[2]*u[3]*v[3]
+end
+
+#=
+function _int41(u::Vector, v::Vector)
+    return -(1/40)*u[1]^2*u[2]*v[1] - 7/240*u[1]*u[2]^2*v[1] - 
+        1/140*u[2]^3*v[1] - 1/45*u[1]^2*u[3]*v[1] - 
+        41/840*u[1]*u[2]*u[3]*v[1] - 1/60*u[2]^2*u[3]*v[1] - 
+        13/630*u[1]*u[3]^2*v[1] - (29*u[2]*u[3]^2*v[1])/2268 - 
+        1/315*u[3]^3*v[1] + 1/40*u[1]^3*v[2] + 7/240*u[1]^2*u[2]*v[2] + 
+        1/140*u[1]*u[2]^2*v[2] + 1/56*u[1]^2*u[3]*v[2] + 
+        1/210*u[1]*u[2]*u[3]*v[2] - 1/756*u[2]^2*u[3]*v[2] - (
+        u[1]*u[3]^2*v[2])/1296 - (19*u[2]*u[3]^2*v[2])/10080 - (
+        u[3]^3*v[2])/1584 + 1/45*u[1]^3*v[3] + 13/420*u[1]^2*u[2]*v[3] + 
+        1/84*u[1]*u[2]^2*v[3] + 1/756*u[2]^3*v[3] + 
+        13/630*u[1]^2*u[3]*v[3] + (41*u[1]*u[2]*u[3]*v[3])/3024 + (
+        19*u[2]^2*u[3]*v[3])/10080 + 1/315*u[1]*u[3]^2*v[3] + (
+        u[2]*u[3]^2*v[3])/1584
+end
+
+function _int42(u::Vector, v::Vector)
+    return -(1/20)*u[1]*u[2]*v[1]^2 - 7/240*u[2]^2*v[1]^2 - 
+        2/45*u[1]*u[3]*v[1]^2 - 41/840*u[2]*u[3]*v[1]^2 - 
+        13/630*u[3]^2*v[1]^2 + 1/20*u[1]^2*v[1]*v[2] - 
+        1/70*u[2]^2*v[1]*v[2] - 11/840*u[1]*u[3]*v[1]*v[2] - 
+        1/35*u[2]*u[3]*v[1]*v[2] - (41*u[3]^2*v[1]*v[2])/3024 + 
+        7/240*u[1]^2*v[2]^2 + 1/70*u[1]*u[2]*v[2]^2 + 
+        1/210*u[1]*u[3]*v[2]^2 - 1/378*u[2]*u[3]*v[2]^2 -
+        (19*u[3]^2*v[2]^2)/10080 + 2/45*u[1]^2*v[1]*v[3] + 
+        11/840*u[1]*u[2]*v[1]*v[3] - 1/210*u[2]^2*v[1]*v[3] -
+        (109*u[2]*u[3]*v[1]*v[3])/9072 - 2/315*u[3]^2*v[1]*v[3] + 
+        41/840*u[1]^2*v[2]*v[3] + 1/35*u[1]*u[2]*v[2]*v[3] + 
+        1/378*u[2]^2*v[2]*v[3] + (109*u[1]*u[3]*v[2]*v[3])/9072 - 
+        1/792*u[3]^2*v[2]*v[3] + 13/630*u[1]^2*v[3]^2 +
+        (41*u[1]*u[2]*v[3]^2)/3024 + (19*u[2]^2*v[3]^2)/10080 + 
+        2/315*u[1]*u[3]*v[3]^2 + 1/792*u[2]*u[3]*v[3]^2
+end
+=#
+
+
 """
 Main function for performing quantum annealing simulation via a Magnus Expansion (second order).
 Noise can be simulated by running multiple times with randomized constant fields.
@@ -454,6 +628,7 @@ function _hamiltonian_sum(h_list::Vector)
             push!(h_sum, item)
         end
     end
+    println(length(h_sum))
     return h_sum
 end
 
@@ -482,18 +657,19 @@ function _bernoulli_num_fact(n)
             A[j] = j * (A[j] - A[j + 1])
         end
     end
-    return float(A[1]/factorial(n))
+    return Float64(A[1]/factorial(n))
 end
 
 # https://en.wikipedia.org/wiki/Magnus_expansion
 # Initial H is a polynomal of X and Z
 function _magnus_generator(H::Vector, order::Int)
     @assert(order >= 1)
-    Ω_list = [_hamiltonian_integrate(H)]
 
+    Ω_list = [_hamiltonian_integrate(H)]
     S_list = Dict()
 
     for n in 2:order
+        #println("order: $(n)")
         S_list[(1,n)] = _hamiltonian_commutator(Ω_list[n-1], H)
         for j in 2:n-1
             S_list[(j,n)] = _hamiltonian_sum([
@@ -504,6 +680,7 @@ function _magnus_generator(H::Vector, order::Int)
         Ω_n = _hamiltonian_sum([
             _hamiltonian_scalar(_bernoulli_num_fact(j),_hamiltonian_integrate(S_list[(j,n)]))
         for j in 1:n-1])
+
         push!(Ω_list, Ω_n)
     end
 
@@ -603,6 +780,10 @@ function simulate(ising_model::Dict, annealing_time::Real, annealing_schedule::A
         ]
 
         Ω_list = _magnus_generator(H, order)
+        #for (i,Ωi) in enumerate(Ω_list)
+        #    println("Ω_$(i)")
+        #    display(Matrix(_hamiltonian_eval(δs, Ωi)))
+        #end
         Ω = sum(_hamiltonian_eval(δs, Ωi) for Ωi in Ω_list)
 
         U_next = exp(Matrix(Ω))
